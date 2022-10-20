aa0582af202ec78da34934760862a7ac75cfc4ce
ericvini000 <ericviniciusfelixsilva@gmail.com>
2022-10-20 16:01:27 -0300

    Entrega(Corrigida)

--- script.js
+++ script.js
@@ -3,25 +3,35 @@ let num1 = parseInt(prompt(`Insira um numero:`));
 let num2 = parseInt(prompt(`Insira outro numero:`));
 
 if (num1 > num2){
+
     let diferenca = num1 - num2;
     alert(`O maior número é ${num1} e a diferença é de ${diferenca}`);
+
 } else if (num2 > num1){
+
     let diferenca = num2 - num1;
     alert(`O maior número é ${num2} e a diferença é de ${diferenca}`);
+
 } else{
+
     alert("Os valores são iguais")
 }
 
 // Exercício 2
-let num3 = parseFloat(prompt("Fale um número:"));
-let num4 = parseFloat(prompt("Fale um número:"));
+let num3 = parseInt(prompt("Fale um número:"));
+let num4 = parseInt(prompt("Fale um número:"));
 
 if (num3 == num4){
+
     alert('Os números são iguais');
+
 } else if(num3 > num4){
+
     alert(`O número ${num3} é maior que o numero ${num4}`);
+
 } else{
-    alert("Os números são iguais");
+
+    alert(`O numero ${num4} é maior que o numero ${num3}`);
 }
 
 // Exercício 3
@@ -30,8 +40,11 @@ let prestacao = prompt("Qual o valor da prestação?");
 let calculoSal = (salario * 30)/100;  
 
 if (prestacao > calculoSal){
+    
     alert(`Empréstimo não concedido`);
+
 } else{
+
     alert("Empreéstimo concedido");
 }
 
@@ -39,6 +52,7 @@ if (prestacao > calculoSal){
 let num5 = prompt("Escreva um numero:");
 
 if ((num5%3) == 0 && (num5%5) == 0){
+
     alert("Numero Invalido");
 
 } else if((num5%3) == 0 || (num5%5) == 0){
@@ -63,28 +77,38 @@ if ((num5%3) == 0 && (num5%5) == 0){
 let diaDaSemana = parseInt(prompt("Informe um número de 1 a 7"));
 
 if ( diaDaSemana > 0 && diaDaSemana < 8){
+
     if (diaDaSemana == 1){
+
         alert("Domingo");
-    }
-    else if(diaDaSemana == 2 ){
+
+    }else if(diaDaSemana == 2 ){
+
         alert("Segunda-feira");
-    }
-    else if (diaDaSemana == 3 ){
+
+    }else if (diaDaSemana == 3 ){
+
         alert("Terça-feira");
+
     }
-    else if (diaDaSemana == 4 ){
+    else if(diaDaSemana == 4 ){
+
         alert("Quarta-feira");
-    }
-    else if(diaDaSemana == 5 ){
+
+    }else if(diaDaSemana == 5 ){
+
         alert("Quinta-feira");
-    }
-    else if(diaDaSemana == 6 ){
+
+    }else if(diaDaSemana == 6 ){
+
         alert("Sexta-feira");
-    }
-    else {
+
+    }else {
         alert("Sábado");
     }
+
 } else{
+
     alert("Erro");
 }
 
@@ -93,13 +117,17 @@ let salario1 = parseFloat(prompt("Insira seu salário:"));
 let reajuste = parseFloat(prompt("Insira seu reajuste de salário:"));
 
 if(reajuste <= 5 && reajuste >= 1){
+
     let novosalario = salario1 +(salario1 * reajuste/100);
     alert(`Com o reajuste o salário ficará: ${novosalario}`);
+    
 } else{
+
     alert("Erro, o reajuste não esta dentro do limite");
 }
 
 // Exercício 7
+
 let dolar = parseFloat(prompt("Converta um valor de dolar($) pra real(R$):"));
 let cotacao = parseFloat(prompt("Fale a cotacao do real atual:"));
 let valorconvertido = parseFloat(dolar * cotacao);
@@ -107,8 +135,9 @@ let valorconvertido = parseFloat(dolar * cotacao);
 alert(`O valor de $${dolar} em real ficará R$${valorconvertido}, tendo ${cotacao} como cotação`);
 
 // Exercício 8
+
 let tipo = parseInt(prompt("Escolha o tipo de pulverização de 1 a 4"));
-let area = parseFloat(prompt("Informe sua área em Acre(s):"));
+let area = parseInt(prompt("Informe sua área em Acre(s):"));
 let custo1 = 50 * area;
 let custo2 = 100 * area;
 let custo3 = 150 * area;
@@ -129,10 +158,14 @@ if (tipo <= 4 && tipo >= 1){
 
                 custo1 = custo1 - ((custo1-750)*10/100)
                 alert(`O valor a ser pago é de R$${custo1}`)
+
             }
+
         }else {
+
             alert(`Você pagará R$${custo1}`)
         }
+
     }else if(tipo == 2){
 
         if(area > 1000 || custo2 > 750){
@@ -149,10 +182,11 @@ if (tipo <= 4 && tipo >= 1){
             }
 
         }else {
+
             alert(`Você pagará R$${custo2}`)
         }
 
-    } else if(tipo == 3){
+    }else if(tipo == 3){
 
         if(area > 1000 || custo3 > 750){
 
@@ -166,11 +200,13 @@ if (tipo <= 4 && tipo >= 1){
                 custo3 = custo3 - ((custo3-750)*10/100)
                 alert(`O valor a ser pago é de R$${custo3}`)
             }
+
         }else {
+
             alert(`Você pagará R$${custo3}`)
         }
 
-    } else{
+    }else{
 
         if(area > 1000 || custo4 > 750){
 
@@ -184,12 +220,16 @@ if (tipo <= 4 && tipo >= 1){
                 custo4 = custo4 - ((custo4-750)*10/100)
                 alert(`O valor a ser pago é de R$${custo4}`)
             }
+
         }else {
+
             alert(`Você pagará R$${custo4}`)
         }
 
     }
+
 }else{
+    
     alert("Erro, por favor insira um numero entre 1 e 4")
 }
 
@@ -198,6 +238,7 @@ let tempo = prompt("Quanto tempo durou a viagem?");
 let velocidade = prompt("Qual foi a velocidade durante a viagem?");
 let distancia = tempo *velocidade;
 let litro = distancia /12;
+
 litro = litro.toFixed(2)
 
 alert(`O seu veículo percorreu ${distancia} a ${velocidade} em ${tempo} e consumiu ${litro}l de gasolina`)
@@ -208,11 +249,11 @@ let taxa = parseInt(prompt("Qual a taxa da prestação"));
 let tempo1 = parseInt(prompt("Tempo de atraso:"));
 let prestacao2 = valor + (valor* (taxa/100) *tempo1)
 
-if(tempo1 != 0 || tempo1 != parseFloat(tempo1)){
+if(tempo1 != 0 || tempo1 != parseInt(tempo1)){
 
     alert(`O valor é de R$${prestacao2}`)
     
-} else{
+}else{
     
     alert("Valor inválido o tempo de atraso deve ser em dias.")
 }
